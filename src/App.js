b55beb0bac033e2d2e0bc482d9a9ee064d54f7ba
Fulya <fulya.ertay@gmail.com>
2023-01-01 18:31:27 +0300

    feat:fetch cities by axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 
 import './App.css';
-
+import Cities from './components/cities';
 function App() {
   return (
     <div className="App">
+      <Cities></Cities>
   
     </div>
   );
--- src/components/cities.js
+++ src/components/cities.js
@@ -1,13 +1,25 @@
 import React,{useEffect} from 'react'
-
-export default function cities() {
+import axios from 'axios';
+export default function Cities() {
     //when dom is first loaded, call getCities() function
     useEffect(()=>{
         getCities();
     },[])
-    const getCities=async()=>{
-        //reach API
-        const response=await fetch('http://worldtimeapi.org/timezones');
+    // const getCities=async()=>{
+    //     //reach API
+    //     const response=await fetch('http://worldtimeapi.org/api/timezones');
+    //    // console.log(response);
+    //     const cities=await response.json();
+    //     console.log(cities);
+    // }   
+
+    //fetch with axios packages
+    const getCities=()=>{
+        axios.get('http://worldtimeapi.org/api/timezones')
+        .then(res=>{
+            console.log(res.data);
+        })
+
     }
   return (
     <div>cities</div>
