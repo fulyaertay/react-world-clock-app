2c28730832d1c9acf6ed14500bbd886f5b69a7fa
Fulya <fulya.ertay@gmail.com>
2023-01-01 18:48:36 +0300

    feat: add materialize css on cities component

--- src/components/cities.js
+++ src/components/cities.js
@@ -1,6 +1,7 @@
-import React,{useEffect} from 'react'
+import React,{useEffect,useState} from 'react'
 import axios from 'axios';
 export default function Cities() {
+    const [cities,setCities]=useState([]);
     //when dom is first loaded, call getCities() function
     useEffect(()=>{
         getCities();
@@ -18,10 +19,21 @@ export default function Cities() {
         axios.get('http://worldtimeapi.org/api/timezones')
         .then(res=>{
             console.log(res.data);
+            setCities(res.data);
         })
 
     }
   return (
-    <div>cities</div>
+    <div className='row'>
+        {cities.map(s=>(
+            <div className='col l3 m4 s6' key={s}>
+                <div className='card blue-grey darken-1'>
+                    <div className='card-content white-text'  >
+                        <p>{s}</p>
+                    </div>
+                </div> 
+            </div>
+        ))}
+    </div>
   )
 }
