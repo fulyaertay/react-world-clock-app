60b1109df9804e6d799201087dadaf59f766758c
Fulya <fulya.ertay@gmail.com>
2023-01-01 21:50:05 +0300

    feat:material icon

--- src/components/clock.js
+++ src/components/clock.js
@@ -1,12 +1,24 @@
-import React,{useEffect} from 'react'
-
+import React,{useEffect, useState} from 'react'
+import axios from 'axios';
+import {Link} from 'react-router-dom';
 export default function Clock({match}) {
+    const [clock,setClock]=useState('');
     useEffect(()=>{
-        console.log(match);
-    })
- 
+        getClock();
+    },[])
+    const getClock=()=>{
+        axios.get(`http://worldtimeapi.org/api/timezone/${match.params.region}/${match.params.place}`)
+        .then(res=>{
+            //console.log(res.data);
+            setClock(res.data.datetime);
+        })
+    }
  
     return (
-    <div>clock</div>
+    <div className="container">
+        <div className="card">
+            <Link to={'/'} className="btn-floating"><i className='material-icons'>undo</i></Link>
+        </div>
+    </div>
   )
 }
