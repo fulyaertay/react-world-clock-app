c5fe26cbad8656a084092e14d6622da19e80cc19
Fulya <fulya.ertay@gmail.com>
2023-01-01 18:16:30 +0300

    feat:reach API

--- src/components/cities.js
+++ src/components/cities.js
@@ -1,6 +1,14 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 
 export default function cities() {
+    //when dom is first loaded, call getCities() function
+    useEffect(()=>{
+        getCities();
+    },[])
+    const getCities=async()=>{
+        //reach API
+        const response=await fetch('http://worldtimeapi.org/timezones');
+    }
   return (
     <div>cities</div>
   )
